185cef8e66c470154ab798afe9d5037d2f75e2ba
Deven <devend711@gmail.com>
2014-11-16 01:12:41 -0500

    change for mongolab

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
 module.exports.portNum = process.env.PORT || 3000;
 module.exports.mongoPort = 27017;
-module.exports.mongoURI = (process.env.MONGOLAB_URI ? ('mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@ds051630.mongolab.com:51630/heroku_app31452963') :  ('localhost'));
+module.exports.mongoURI = (process.env.MONGOLAB_URI ? ('mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@ds053090.mongolab.com:53090/heroku_app31656433') :  ('localhost'));
